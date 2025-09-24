3272ae63ac6c421dbce1824956fe1bd6ae96484f
Dhegnaas <abdirahman460930@gmail.com>
2025-09-24 16:25:19 +0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import abdi from "./assets/image.png"; // sax .png
 
 const App = () => {
   return (
@@ -6,10 +7,14 @@ const App = () => {
       <div className="pattern"></div>
       <div className="wrapper">
         <header>
+          <img src={abdi} alt="ABDIRAHMAN-1 Banner" />
           <h1>
-            Find <span className="text-gradient">Movies</span> you'll enjoy without the hassle
+            Find <span className="text-gradient">Movies</span> you'll enjoy
+            without the hassle
           </h1>
         </header>
+
+        <p>Search</p>
       </div>
     </main>
   );
